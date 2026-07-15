0d61bf7a6e8c5a39d472d8f51ad8c2312ff38e6e
Akbartrader <managerbos1602@gmail.com>
2026-07-15 14:57:34 +0800

    add

--- js/versi-v01/benner-promo.js
+++ js/versi-v01/benner-promo.js
@@ -1,7 +1,7 @@
 const gcsHalamanPromoBannerData = [
     {
         image: "https://res.cloudinary.com/dutuhbbg2/image/upload/v1784097642/ChatGPT_Image_15_Jul_2026_14.39.32_yp5ijn.png",
-        link: "promo-detail-1.html",
+        link: "#",
         start: "2026-07-01 00:00:00",
         end: "2027-07-31 23:59:59"
     }
